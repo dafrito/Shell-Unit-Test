fd0a3b8efb9c453ebb87bf6c5a9bb95ac04720d6
Aaron Faanes <dafrito@gmail.com>
2010-05-20 14:56:07 -0500

    tst-keeps actually cats to the right dir

--- tests/tst-keeps.sh
+++ tests/tst-keeps.sh
@@ -1,7 +1,8 @@
 mkdir tests
-cat >always-passing.sh <<EOF
+cat >tests/always-passing.sh <<EOF
 true
 EOF
 
 tst -k
 [ -e results ] || die "tst must respect -k and keep tests";
+[ -e results/1 ] || die "tst must respect -k and keep even passing tests";
