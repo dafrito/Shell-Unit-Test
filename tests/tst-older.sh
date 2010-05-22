e3681c23308a1b5c637d7b7e42bf727334d90566
Aaron Faanes <dafrito@gmail.com>
2010-05-22 10:02:50 -0500

    Fixed an omitted 'die' in tst-older

--- tests/tst-older.sh
+++ tests/tst-older.sh
@@ -4,6 +4,6 @@ false
 EOF
 
 tst 2>&1
-[ ! -e results/older ] || "older directory must not be spuriously created";
+[ ! -e results/older ] || die "older directory must not be spuriously created";
 tst 2>&1
 [ -d results/older ] || die "older directory must be created for subsequent runs";
