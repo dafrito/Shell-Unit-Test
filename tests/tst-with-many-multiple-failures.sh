cdcf9c635de6d0b55a335a4a26111b091c0d8b73
Aaron Faanes <dafrito@gmail.com>
2010-05-22 09:48:43 -0500

    timing-dependent test now tries to get a good run for 10 times

--- tests/tst-with-many-multiple-failures.sh
+++ tests/tst-with-many-multiple-failures.sh
@@ -4,15 +4,24 @@ cat >always-passing.sh <<EOF
 true
 EOF
 
-START=`date +%S`
-echo First test creates results folder
-tst -k
-echo Second test creates results/older
-tst -k
-echo "Third test creates results/older/results-<date>.1"
-tst -k
-FINISH=`date +%S`
-[ $START = $FINISH ] || die "This test must occur within the same second";
+CHOKE=0
+while true; do
+	[ $CHOKE -lt 10 ] || die "Test would not complete in 10 tries";
+	START=`date +%S`
+	echo First test creates results folder
+	tst -k
+	echo Second test creates results/older
+	tst -k
+	echo "Third test creates results/older/results-<date>.1"
+	tst -k
+	FINISH=`date +%S`
+	if [ $START = $FINISH ]; then
+		break;
+	else
+		tst c
+	fi
+	let CHOKE++
+done
 
 cd ..
 [ `find * -maxdepth 0 -name 'results*' | wc -l` = 1 ] || die "Multiple results were created";
